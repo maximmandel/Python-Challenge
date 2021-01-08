07927481194e126bbb6e9d7553b8c05fb31fed54
maximmandel <maximmandel@gmail.com>
2021-01-07 22:51:19 -0600

    1st homeowrk draft

---
+++ PyBank/main.py
@@ -0,0 +1,76 @@
+# Input location
+import os
+import csv
+
+# file location
+
+file_to_read = 'Resources/PyBankPLData.csv'
+
+# Create lists for variables
+
+months = []
+total_profit = []
+monthly_profit_change = []
+
+# open w context
+with open(file_to_read, newline="", encoding="utf-8") as budget:
+
+    # store contents of csv in variable csv reader
+    csv_reader = csv.reader(budget)
+
+    # skip the header labels
+    header = next(csv_reader)
+
+    # iterate through rows
+    for row in csv_reader:
+
+        # append total months and total profit
+        months.append(row[0])
+        total_profit.append(int(row[1]))
+
+# iterate through rofits for profit change by month
+for i in range(len(total_profit)-1):
+
+    # difference added to change
+    monthly_profit_change.append(total_profit[i+1]-total_profit[i])
+
+# get max/min for profit change
+largest_increase = max(monthly_profit_change)
+largest_decrease = min(monthly_profit_change)
+
+#recorrelate max/min to correct month
+largest_increase_month = monthly_profit_change.index(largest_increase) + 1
+largest_decrease_month = monthly_profit_change.index(largest_decrease) + 1
+
+total_profit_sum = sum(total_profit)
+
+total_months = len(months)
+
+#printing
+print("Financial Analysis")
+print("--------------")
+print("Months " + str(total_months))
+print(f"Total Profit {total_profit_sum}")
+
+# print(total: $(sum("total_profit"))
+print(f"Average Change: {round(sum(monthly_profit_change)/len(monthly_profit_change),2)}")
+print(f"Largest Profit Increase: {months[largest_increase_month]}(${(str(largest_increase))}")
+print(f"Largest Profit Decrease: {months[largest_decrease_month]}(${(str(largest_decrease))}")
+
+#output 1
+file = 'Python-Challenge\PyBank\Financial_analysis.txt'
+#output 2
+file.write("Financial Analysis")
+file.write("\n")
+file.write("--------------------")
+file.write("\n")
+file.write(f"Months: {len(months)}")
+file.write("\n")
+file.write(f"total: ${sum(total_profit)}")
+file.write("\n")
+file.write(f"Average Change: {round(sum(monthly_profit_change)/len(monthly_profit_change),2)}")
+file.write("\n")
+file.write(f"Greatest Increase in Profits: {months[max_increase_month]} (${(str(max_increase_value))})")
+file.write("\n")
+file.write(f"Greatest Decrease in Profits: {months[max_decrease_month]} (${(str(max_decrease_value))})")
+file.close()
---
+++ PyPoll/main.py
@@ -0,0 +1,93 @@
+# Input location
+import os
+import csv
+
+#file location
+file_to_read = os.path.join('Resources/PyPollPLData.csv')
+
+print(file_to_read)
+
+# # of total votes
+
+total_votes = 0
+vote_khan = 0
+vote_correy = 0
+vote_li = 0
+vote_otooley = 0
+
+# open w context
+with open(file_to_read, newline="", encoding="utf-8") as elections:
+
+    # store contents of csv in variable csv reader
+    csv_reader = csv.reader(elections, delimiter=",")
+
+    # skip the header labels
+    header = next(csvreader)
+
+    # iterate through rows
+    for row in csvreader:
+
+        # count voter ID
+        total_votes +=1
+
+        # Count names
+        if row [2] == "Khan":
+            vote_khan +=1
+        elif row [2] == "Correy":
+            vote_correy +=1
+        elif row [2] == "Li":
+            vote_li +=1
+        elif row [2] == "Otooley":
+            vote_otooley +=1
+
+#creat dict
+candidates = ["Khan","Correy","Li","Otooley"]
+votes = [vote_khan,vote_correy,vote_li,vote_otooley]
+
+# join lists and find winner
+dict_candivotes = dict(zip(candidates,votes))
+key = max(dict_candivotes, key=dict_candivotes.get)
+
+#summarized
+percent_khan = (vote_khan/total_votes) * 100
+percent_correy = (vote_correy/total_votes) * 100
+percent_li = (vote_li/total_votes) * 100
+percent_otooley = (vote_otooley/total_votes) * 100
+
+#printing
+print(f"Election Results")
+print(f"------------")
+print(f"Total Votes: {total_votes}")
+print(f"------------")
+print(f"Khan: {percent_khan:.3f}% ({vote_khan})")
+print(f"Correy: {percent_correy:.3f}% ({vote_correy})")
+print(f"Li: {percent_li:.3f}% ({vote_li})")
+print(f"Otooley: {percent_otooley:.3f}% ({vote_otooley})")
+print(f"------------")
+print(f"Winner: {key}")
+print(f"------------")
+
+# output
+output_file = path.("Python-Challenge","PyPoll", "Election_results_Summary.txt")
+with open(output_file, "w") as file:
+# write
+    file.write(f"Election Results")
+    file.write("\n")
+    file.write(f"------------")
+    file.write("\n")
+    file.write(f"total votes: {total_votes}")
+    file.write("\n")
+    file.write(f"------------")
+    file.write(f"Khan: {percent_khan:.3f}%({vote_khan})")
+    file.write("\n")
+    file.write(f"Correy: {percent_correy:.3f}%({vote_correy})")
+    file.write("\n")
+    file.write(f"Li: {percent_li:.3f}%({vote_li})")
+    file.write("\n")
+    file.write(f"Otooley: {percent_otooley:.3f}%({vote_otooley})")
+    file.write("\n")
+    file.write(f"------------")
+    file.write("\n")
+    file.write(f"winner: {key}")
+    file.write("\n")
+    file.write(f"------------")
